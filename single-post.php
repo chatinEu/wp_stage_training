d57c0ff9eaf64074f3e9568be220aee4fbb8070c
chatinEu <eudes.chatin@gmail.com>
2022-04-06 14:49:31 +0200

    queries to db

--- single-post.php
+++ single-post.php
@@ -10,11 +10,31 @@ hello world
         while (have_posts()):
             the_post();?>
             
-                <h5><?php the_title();?></h5>
-                <p> <?php the_content();?> </p>
-                <!--a href="#" class="btn btn-primary">Go somewhere</a>!-->
-            
-        <?php
+            <h5><?php the_title();?></h5>
+            <p> <?php the_content();?> </p>
+            <!--a href="#" class="btn btn-primary">Go somewhere</a>!-->
+        
+    
+
+            <h2> Articles relatifs</h2>
+            <div class="row">
+                <?php   
+
+                $query = new WP_Query(array(
+                    'post__not_in' => [get_the_ID()],
+                    'post_type' => 'post',
+                    'post_per_page' => 3
+                ));
+                while ($query -> have_posts()):
+                    $query -> the_post();
+                    the_title();
+                    get_template_part('parts\bien.php','bien');
+                endwhile;
+                //important t
+                wp_reset_postdata();
+
+            echo'</div>';
+                
         endwhile;
     else: ?>
     <h1>Pas d'articles à afficher</h1>
